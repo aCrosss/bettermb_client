c8073dee9b254b65b1a04c88ab9065558f443545
Alexey Krestianinov <akrestianinov@akber-soft.ru>
2026-02-19 12:16:51 +0500

    fix exception code validation; format mb_base.c

--- src/mb_base.c
+++ src/mb_base.c
@@ -158,6 +158,14 @@ is_valid_ex_code(mb_ex_t exc) {
     }
 }
 
+//    ########  ##     ## #### ##       ########  #### ##    ##  ######
+//    ##     ## ##     ##  ##  ##       ##     ##  ##  ###   ## ##    ##
+//    ##     ## ##     ##  ##  ##       ##     ##  ##  ####  ## ##
+//    ########  ##     ##  ##  ##       ##     ##  ##  ## ## ## ##   ####
+//    ##     ## ##     ##  ##  ##       ##     ##  ##  ##  #### ##    ##
+//    ##     ## ##     ##  ##  ##       ##     ##  ##  ##   ### ##    ##
+//    ########   #######  #### ######## ########  #### ##    ##  ######
+
 // ======================================================================================
 // Payloads
 // ======================================================================================
@@ -341,114 +349,6 @@ build_adu(u8 *adu, frame_t *frame) {
     }
 
     return RC_FAIL;
-}
-
-int
-check_req_rsp_pdu(u8 *req, u8 req_len, u8 *rsp, u8 rsp_len) {
-    u16 qty    = 0;
-    u8  nbytes = 0;
-    u8  fcode  = req[0];
-
-    // check for valid modbus exception code
-    if (rsp[0] & 0x80) {
-        // fc not equal anyway
-        if ((fcode != rsp[0]) & (fcode != 0x80)) {
-            log_linef("! exception code; req-rsp fc not same (req: %d, rsp: %d):", fcode, rsp[0] & !0x80);
-            return RC_FAIL;
-        }
-
-        mb_ex_t exc = rsp[1];
-        if (is_valid_ex_code(exc)) {
-            log_linef("! slave returned valid exception code: %s:", str_ex_code(exc));
-            return RC_FAIL;
-        } else {
-            log_linef("! slave returned invalid exception code: 0x%02X:", exc);
-            return RC_FAIL;
-        }
-    }
-
-    // function codes must be equal
-    if (req[0] != rsp[0]) {
-        log_linef("! req-rsp fc not same (req: %d, rsp: %d):", fcode, rsp[0]);
-        return RC_FAIL;
-    }
-
-    switch (fcode) {
-    case MB_FC_READ_COILS:
-    case MB_FC_READ_DISCRETE_INPUTS:
-        qty    = req[3] << 8 | req[4];
-        nbytes = (qty + 7) / 8;
-
-        // if (rsp[1] != nbytes) {
-        //     log_linef("! expected-recieved byte count doesn't match (req: %d, rsp: %d):", nbytes, rsp[1]);
-        //     return RC_FAIL;
-        // }
-
-        if (rsp_len != nbytes + 2) {
-            log_linef(
-              "  ^ expected-recieved response len doesn't match (expected: %d, recieved: %d):", rsp_len, nbytes + 2);
-            return RC_FAIL;
-        }
-
-        break;
-
-    case MB_FC_READ_HOLDING_REGISTERS:
-    case MB_FC_READ_INPUT_REGISTERS:
-        qty    = req[3] << 8 | req[4];
-        nbytes = qty * 2;
-
-        if (rsp[1] != nbytes) {
-            log_linef("! expected-recieved byte count doesn't match (req: %d, rsp: %d):", fcode, rsp[0]);
-            return RC_FAIL;
-        }
-
-        if (rsp_len != nbytes + 2) {
-            log_linef(
-              "! expected-recieved response len doesn't match (expected: %d, recieved: %d):", rsp_len, nbytes + 2);
-            return RC_FAIL;
-        }
-
-        break;
-
-    // for those functions normal rsp is echo of req
-    case MB_FC_WRITE_SINGLE_COIL:
-    case MB_FC_WRITE_SINGLE_REGISTER:
-        if (memcmp(req, rsp, req_len) != 0) {
-            log_linef("! expected echo of request:");
-            return RC_FAIL;
-        }
-
-        break;
-
-    // normal rsp return function code, starting address and qaunity of outputs,
-    // so first 5 bytes have to be equal
-    case MB_FC_WRITE_MULTIPLE_COILS:
-    case MB_FC_WRITE_MULTIPLE_REGISTERS:
-        if (memcmp(req, rsp, 5) != 0) {
-            log_linef("! expected echo of firtst 5 bytes of request (fc, starting addr and qty of outputs):");
-            return RC_FAIL;
-        }
-
-        break;
-    case MB_FC_WRITE_AND_READ_REGISTERS:
-        qty    = req[3] << 8 | req[4];
-        nbytes = qty * 2;
-
-        if (rsp[1] != nbytes) {
-            log_linef("! expected-recieved byte count doesn't match (req: %d, rsp: %d):", fcode, rsp[0]);
-            return RC_FAIL;
-        }
-
-        if (rsp_len != nbytes + 2) {
-            log_linef(
-              "! expected-recieved response len doesn't match (expected: %d, recieved: %d):", rsp_len, nbytes + 2);
-            return RC_FAIL;
-        }
-
-        break;
-    }
-
-    return RC_SUCCESS;
 }
 
 // =============================================================================
@@ -801,6 +701,127 @@ mb_extract_frame(mb_protocol_t proto, u8 *adu, int adu_len, frame_t *out) {
     case MB_PROTOCOL_ASCII: mb_ascii_extract_frame(adu, adu_len, out); break;
     case MB_PROTOCOL_TCP: mb_tcp_extract_frame(adu, adu_len, out); break;
     }
+}
+
+// ##     ##    ###    ##       #### ########     ###    ######## ####  #######  ##    ##
+// ##     ##   ## ##   ##        ##  ##     ##   ## ##      ##     ##  ##     ## ###   ##
+// ##     ##  ##   ##  ##        ##  ##     ##  ##   ##     ##     ##  ##     ## ####  ##
+// ##     ## ##     ## ##        ##  ##     ## ##     ##    ##     ##  ##     ## ## ## ##
+//  ##   ##  ######### ##        ##  ##     ## #########    ##     ##  ##     ## ##  ####
+//   ## ##   ##     ## ##        ##  ##     ## ##     ##    ##     ##  ##     ## ##   ###
+//    ###    ##     ## ######## #### ########  ##     ##    ##    ####  #######  ##    ##
+
+// =============================================================================
+// Validate Response against Request
+// =============================================================================
+
+int
+check_req_rsp_pdu(u8 *req, u8 req_len, u8 *rsp, u8 rsp_len) {
+    u16 qty    = 0;
+    u8  nbytes = 0;
+    u8  fcode  = req[0];
+
+    // check for valid modbus exception code
+    if (rsp[0] & 0x80) {
+        u8 rsp_fcode = rsp[0] & ~0x80;
+        // fc not equal anyway
+        if ((fcode != rsp[0]) & (fcode != rsp_fcode)) {
+            log_linef("! exception code; req-rsp fc not same (req: %d, rsp: %d):", fcode, rsp_fcode);
+            return RC_FAIL;
+        }
+
+        mb_ex_t exc = rsp[1];
+        if (is_valid_ex_code(exc)) {
+            log_linef("! slave returned valid exception code: %s:", str_ex_code(exc));
+            return RC_FAIL;
+        } else {
+            log_linef("! slave returned invalid exception code: 0x%02X:", exc);
+            return RC_FAIL;
+        }
+    }
+
+    // function codes must be equal
+    if (req[0] != rsp[0]) {
+        log_linef("! req-rsp fc not same (req: %d, rsp: %d):", fcode, rsp[0]);
+        return RC_FAIL;
+    }
+
+    switch (fcode) {
+    case MB_FC_READ_COILS:
+    case MB_FC_READ_DISCRETE_INPUTS:
+        qty    = req[3] << 8 | req[4];
+        nbytes = (qty + 7) / 8;
+
+        // if (rsp[1] != nbytes) {
+        //     log_linef("! expected-recieved byte count doesn't match (req: %d, rsp: %d):", nbytes, rsp[1]);
+        //     return RC_FAIL;
+        // }
+
+        if (rsp_len != nbytes + 2) {
+            log_linef(
+              "  ^ expected-recieved response len doesn't match (expected: %d, recieved: %d):", rsp_len, nbytes + 2);
+            return RC_FAIL;
+        }
+
+        break;
+
+    case MB_FC_READ_HOLDING_REGISTERS:
+    case MB_FC_READ_INPUT_REGISTERS:
+        qty    = req[3] << 8 | req[4];
+        nbytes = qty * 2;
+
+        if (rsp[1] != nbytes) {
+            log_linef("! expected-recieved byte count doesn't match (req: %d, rsp: %d):", fcode, rsp[0]);
+            return RC_FAIL;
+        }
+
+        if (rsp_len != nbytes + 2) {
+            log_linef(
+              "! expected-recieved response len doesn't match (expected: %d, recieved: %d):", rsp_len, nbytes + 2);
+            return RC_FAIL;
+        }
+
+        break;
+
+    // for those functions normal rsp is echo of req
+    case MB_FC_WRITE_SINGLE_COIL:
+    case MB_FC_WRITE_SINGLE_REGISTER:
+        if (memcmp(req, rsp, req_len) != 0) {
+            log_linef("! expected echo of request:");
+            return RC_FAIL;
+        }
+
+        break;
+
+    // normal rsp return function code, starting address and qaunity of outputs,
+    // so first 5 bytes have to be equal
+    case MB_FC_WRITE_MULTIPLE_COILS:
+    case MB_FC_WRITE_MULTIPLE_REGISTERS:
+        if (memcmp(req, rsp, 5) != 0) {
+            log_linef("! expected echo of firtst 5 bytes of request (fc, starting addr and qty of outputs):");
+            return RC_FAIL;
+        }
+
+        break;
+    case MB_FC_WRITE_AND_READ_REGISTERS:
+        qty    = req[3] << 8 | req[4];
+        nbytes = qty * 2;
+
+        if (rsp[1] != nbytes) {
+            log_linef("! expected-recieved byte count doesn't match (req: %d, rsp: %d):", fcode, rsp[0]);
+            return RC_FAIL;
+        }
+
+        if (rsp_len != nbytes + 2) {
+            log_linef(
+              "! expected-recieved response len doesn't match (expected: %d, recieved: %d):", rsp_len, nbytes + 2);
+            return RC_FAIL;
+        }
+
+        break;
+    }
+
+    return RC_SUCCESS;
 }
 
 // =============================================================================
